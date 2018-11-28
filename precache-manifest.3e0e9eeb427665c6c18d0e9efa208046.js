4c5ea3d32a501e77fdb1ab05fead5a09001c505b
Alan Soares <alanrsoars@gmail.com>
2018-11-29 09:14:17 +1300

    Updates

---
+++ precache-manifest.3e0e9eeb427665c6c18d0e9efa208046.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "252e47b7d968605003ad",
+    "url": "/reasonml-game-of-life/static/css/main.06fd0f55.chunk.css"
+  },
+  {
+    "revision": "252e47b7d968605003ad",
+    "url": "/reasonml-game-of-life/static/js/main.252e47b7.chunk.js"
+  },
+  {
+    "revision": "6a80133f936d184c7e0e",
+    "url": "/reasonml-game-of-life/static/js/1.6a80133f.chunk.js"
+  },
+  {
+    "revision": "f7c3508bdd4ddcb70041",
+    "url": "/reasonml-game-of-life/static/js/runtime~main.f7c3508b.js"
+  },
+  {
+    "revision": "6cf2297c279015252a5cc365b494551a",
+    "url": "/reasonml-game-of-life/index.html"
+  }
+];
